b328ae6621891c4a47679c0865a09c1f076ca0e4
Bryan Courtney <bcourtney1029@gmail.com>
2020-11-07 21:25:18 -0600

    bug fix attempt for deploying to heroku

---
+++ client/build/precache-manifest.6a88657515156b2aaab5cae7ccc1a9af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c223360dab60a6700ec61e5df63b441e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30969cfde792cbbfeb3b",
+    "url": "/static/js/2.87d9af70.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.87d9af70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993604dd57b1fe440259",
+    "url": "/static/js/main.41e3c101.chunk.js"
+  },
+  {
+    "revision": "27e7b6d1095f4f8a0aa9",
+    "url": "/static/js/runtime-main.08c4e3d1.js"
+  }
+]);
